6dfb492ee443637acc76f543398ced4dc82b2cd4
korchel <korchel21@gmail.com>
2024-05-06 11:24:05 +0300

    modal edited

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,20 +2,19 @@ import React from 'react';
 
 import { useSelector } from 'react-redux';
 import Table from './Table';
-import { getShown } from '../store/modalSlice';
+
 import Modal from './Modal/Modal';
 import Preview from './Preview';
-import BackDrop from './BackDrop';
+
 import { getDataWasFetched } from '../store/dataSlice';
 
 const Main: React.FC = () => {
-  const showModal = useSelector(getShown);
   const dataWasFetched = useSelector(getDataWasFetched);
   return (
     <div className="container">
       {dataWasFetched ? <Table /> : <Preview />}
-      {showModal && <BackDrop />}
-      {showModal && <Modal />}
+
+      <Modal />
     </div>
   );
 };
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,13 +1,15 @@
 import React, { useRef } from 'react';
 import { useOnClickOutside } from 'usehooks-ts';
+import clsx from 'clsx';
 
 import Form from './Form/Form';
-import { useDispatch } from 'react-redux';
-import { closeModal } from '../../store/modalSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { closeModal, getShown } from '../../store/modalSlice';
 
 const Modal: React.FC = () => {
   const ref = useRef(null);
   const dispatch = useDispatch();
+  const showModal = useSelector(getShown);
 
   const handleClickOutside = (): void => {
     dispatch(closeModal());
@@ -15,10 +17,13 @@ const Modal: React.FC = () => {
   useOnClickOutside(ref, handleClickOutside);
 
   return (
-    <div className='modal' ref={ref}>
-      <h2>Стать партнёром проекта</h2>
-      <Form />
-    </div>
+    <>
+      <div className={clsx('backDrop', { backDrop_shown: showModal })}></div>
+      <div className={clsx('modal', { modal_shown: showModal })} ref={ref}>
+        <h2>Стать партнёром проекта</h2>
+        <Form />
+      </div>
+    </>
   );
 };
 
